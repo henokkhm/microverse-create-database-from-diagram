b17e747e6b5696da6fad772a91619cbcce2cfbbb
Henok Kirubel <henokhm2@gmail.com>
2023-09-05 20:13:45 +0300

    Add invoices table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -17,3 +17,12 @@ CREATE TABLE treatments (
 	type VARCHAR(255) NOT NULL, 
 	name VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE invoices (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    total_amount REAL NOT NULL,
+    generated_at TIMESTAMP NOT NULL,
+    paid_at TIMESTAMP NOT NULL,
+    medical_history_id INT UNIQUE,
+    FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
+);
